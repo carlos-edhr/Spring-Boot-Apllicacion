26fc202637e072bbf557677139fedfddb124908b
Carlos <carlos.edhr@comunidad.unam.mx>
2019-10-29 09:16:07 -0700

    First commit with Spring Boot

---
+++ AdminSystem2/src/test/java/com/carlosehs/AdminSystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosehs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSystem2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
