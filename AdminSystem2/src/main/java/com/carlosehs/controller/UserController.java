347cbdd33b43c42ec3698c0638d0b6cd648c1087
Sylvia <Sylvia@LAPTOP-24G8CTC5>
2019-10-29 15:49:59 -0700

    HTML Create

---
+++ AdminSystem2/src/main/java/com/carlosehs/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.carlosehs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return"index";
+		
+	}
+	
+	@GetMapping("/userForm")
+	public String getUserForm() {
+		return"user-form/user-view";
+	}
+	
+	
+
+}
